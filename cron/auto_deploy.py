de43477e17248a9e1fc217669eaa4547b8e3df6f
Dave Thelen <david.thelen@gmail.com>
2017-02-22 11:38:04 -0500

    removing static credentials

--- cron/auto_deploy.py
+++ cron/auto_deploy.py
@@ -8,23 +8,16 @@ from cvprac.cvp_client_errors import CvpApiError
 
 
 ## TODO: Move all of these to a yaml config file
-CVPHOST = ['10.81.111.8']
-CVPUSER = 'cvpadmin'
-CVPPW = 'cvp123'
-TARGET = 'Ansible'
-IMAGE = 'vEOS-4.17.2F-lab'
-GW = '192.168.1.8'
-SUBNET_MASK = '24'
 ##
 
-def make_configlet(ip_addr):
-    config = '' 
-    config += 'hostname ansible-%s\n' % ip_addr
-    config += 'ip route 0/0 %s\n!\n' % GW
-    config += 'interface Management1\nip address %s/%s' \
-               % (ip_addr, SUBNET_MASK)
+def make_configlet(ip_addr, config):
+    configlet = '' 
+    configlet += 'hostname ansible-%s\n' % ip_addr
+    configlet += 'ip route 0/0 %s\n!\n' % config['gw']
+    configlet += 'interface Management1\nip address %s/%s' \
+               % (ip_addr, config['subnet_mask'])
 
-    return config
+    return configlet
 
 def remove_old_configlet(name, cvpclnt):
     info = cvpclnt.api.get_configlet_by_name(name)
@@ -33,6 +26,7 @@ def remove_old_configlet(name, cvpclnt):
 def deploy_device(clnt, device, target_container, configlets, image=None):
     u_task_id = str(uuid.uuid1())
     # move to target container
+    print "starting to deploy device %s..." % device['key']
     cont_info = clnt.api.get_container_by_name(target_container)
 
     task_info = 'Automated Task ID: %s' % u_task_id
@@ -163,7 +157,7 @@ def main():
                     break 
             #have the netElement need to make the ma1 configlet
             sn = node['systemMacAddress']
-            configlet = make_configlet(node['ipAddress'])
+            configlet = make_configlet(node['ipAddress'], config)
             name = sn + '-MA1-CONFIG'
             try:
                 configlet_key = clnt.api.add_configlet(name, configlet)
