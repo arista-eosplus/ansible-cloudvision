c3c1967e1b709ec5d6606fa0e9e0862aee1a5062
Dave <dt@arista.com>
2017-02-28 11:08:47 -0500

    wrong file name

--- cron/auto_deploy.py
+++ cron/auto_deploy.py
@@ -23,103 +23,6 @@ def remove_old_configlet(name, cvpclnt):
     info = cvpclnt.api.get_configlet_by_name(name)
     cvpclnt.api.delete_configlet(name, info['key'])
 
-def deploy_device(clnt, device, target_container, configlets, image=None):
-    u_task_id = str(uuid.uuid1())
-    # move to target container
-    print "starting to deploy device %s..." % device['key']
-    cont_info = clnt.api.get_container_by_name(target_container)
-
-    task_info = 'Automated Task ID: %s' % u_task_id
-
-    data = {'data': [{
-                      'id': 1,
-                      'info' : task_info,
-                      'infoPreview': task_info,
-                      'action': 'update',
-                      'nodeId': device['key'],
-                      'toId': cont_info['key'],
-                      'fromId': 'undefined_container',
-                      'nodeName': device['fqdn'],
-                      'toName': cont_info['name'],
-                      'toIdType': 'container',
-                      'nodeType': 'netelement',
-                      'childTasks': [],
-                      'parentTask': ''}]}
-    t = None
-    try:
-        print 'attemping to add move containter action'
-        clnt.post('/provisioning/addTempAction.do?format=topology&queryParam=&nodeId=%s' % 'root',
-                  data=data)
-        
-    except CvpApiError as e:
-        if "Data already exists" in str(e):
-            pass
-
-        else:
-            #unknown failure
-            print "CvpAPi Error %s: exiting()..." % str(e)
-    print 'done adding move containter action'    
-    print 'getting proposed list of configlets'
-    #get the proposed list of configlets
-    prop_config = clnt.get('/provisioning/getTempConfigsByNetElementId.do?netElementId=%s' % device['key'])
-    cl = [ c['key'] for c in prop_config['proposedConfiglets']]
-    cnl = [ p['name'] for p in prop_config['proposedConfiglets']]
-    
-    #add in device specific configlets
-    for config in configlets:
-        cl.append(config['key'])
-        cnl.append(config['name'])
- 
-    print 'done getting configlets'
-
-    #apply all configlets to the device
-    data = {'data': [{ 'id': 2, 
-                       'info': task_info,
-                       'infoPreview': task_info,
-                       'note': '',
-                       'action': 'associate',
-                       'nodeType': 'configlet',
-                       'nodeId': '',
-                       'configletList': cl,
-                       'configletNamesList': cnl,
-                       'ignoreConfigletList': [],
-                       'ignoreConfigletNamesList': [],
-                       'configletBuilderList': [],
-                       'configletBuilderNamesList': [],
-                       'ignoreConfigletBuilderList': [],
-                       'ignoreConfigletBuilderNamesList': [],
-                       'toId': device['systemMacAddress'],
-                       'toIdType':  'netelement',
-                       'fromId': '',
-                       'nodeName': '',
-                       'fromName': '',
-                       'toName': device['fqdn'],
-                       'nodeIpAddress': device['ipAddress'],
-                       'nodeTargetIpAddress': device['ipAddress'],
-                       'childTasks': [],
-                       'parentTask': '' }]}
-
-    print 'trying to add configlets'
-    clnt.post('/provisioning/addTempAction.do?format=topology&queryParam=&nodeId=%s' % 'root', data=data)
-    print 'done with temp config add'
-    
-    if image:
-        print 'trying to add image bundle'
-        try:
-            image_bundle = clnt.api.get_image_bundle_by_name(image)
-        except CvpApiError as e:
-            print 'issues with finding image bundle: %s exiting...' % str(e)
-            exit()
-        task = clnt.api.apply_image_to_device(image_bundle, device)
-        print 'done with add action for image'
-
-    else:
-        # we are not using the imbedded save_topology call in the apply_image so we need to do it manually
-        task = clnt.api._save_topology_v2([])
-
-    return task
-
-
 def main():
     parser = argparse.ArgumentParser(description="Auto Deploy CVP Nodes")
     parser.add_argument("--config", action="store", help="config file location",
@@ -137,22 +40,19 @@ def main():
     clnt.connect(config['cvp_host'], config['cvp_user'], config['cvp_pw'])
     #get the devices in the undefined container
 
-    inv = clnt.get('/inventory/getInventory.do?startIndex=0&endIndex=0')
-    try:
-        devices = [ k for k,v in inv['containerList'].iteritems() \
-                    if v == 'Undefined'] 
-    except KeyError:
-        print 'no containers found...exiting...'
-        exit()
+    containers = clnt.api.get_device_container_map()
+    inv = clnt.api.get_inventory()
+
+    devices = [ k for k,v in containers.iteritems() if v == 'Undefined'] 
+
     if len(devices) == 0:
-        msg = 'No devices in undefined container.  Exiting'
-        print msg
+        print 'No devices in undefined container.  Exiting'
         exit()
     else:
         node = None
         for device in devices:
             # get the device config:
-            for element in inv['netElementList']:
+            for element in inv:
                 if element['key'] == device:
                     node = element       
                     break 
@@ -175,15 +75,18 @@ def main():
             #add ma1 configlet to device
             configlet_to_add = {'name':name, 'key':configlet_key}
             task = None
-            task = deploy_device(clnt, node, config['target_container'], 
-                                 [configlet_to_add], config['image'])   
+            print 'Attempting to deploy device %s' % node['systemMacAddress']
             try:
-                print 'attempting to execute task'
+                task = clnt.api.deploy_device(node, config['target_container'], 
+                                          [configlet_to_add], config['image'])   
+                print 'Deploy Device task created'
+                print 'Attempting to execute task'
                 clnt.api.execute_task(task['data']['taskIds'][0])
+                print 'Task executed...'
                
             except CvpApiError as e:
-                print 'error executing task: %s' % str(e)
-            print 'task executed...'
+                print str(e)
+
 
                                                      
 
